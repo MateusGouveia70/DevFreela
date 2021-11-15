740f5a3066e53cb22f2acfb16a34a510687bac1e
MateusGouveia <mateusgouveiadev@gmail.com>
2021-11-15 09:08:32 -0300

    Aprofundando em Action

---
+++ DevFreela.API/Controllers/ProjectsController.cs
@@ -0,0 +1,63 @@
+﻿using DevFreela.API.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevFreela.API.Controllers
+{
+    [Route("api/projects")]
+    public class ProjectsController : ControllerBase
+    {
+        // api/projects?query=net core
+        [HttpGet]
+        public IActionResult Get(string query)
+        {
+            return Ok();
+        }
+
+        [HttpGet("id")]
+        public IActionResult GetById(int id)
+        {
+            // return NotFound();
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] CreateProjectModel inputModel)
+        {
+            if (inputModel.Title.Length > 50)
+            {
+                return BadRequest();
+            }
+
+            // Cadastrar o objeto
+
+            return CreatedAtAction(nameof(GetById), new { id = inputModel.Id }, inputModel);
+        }
+
+        [HttpPut("id")]
+        public IActionResult Put(int id, [FromBody] UpdateProjectModel inputModel)
+        {
+            if (inputModel.Description.Length > 200)
+            {
+                return BadRequest();
+            }
+
+            //Atualizar o objeto
+
+            return NoContent();
+        }
+
+        [HttpDelete("id")]
+        public IActionResult Delete(int id)
+        {
+            // Buscar, se não existir, retornar NotFound
+
+            // Remover
+
+            return NoContent();
+        }
+    }
+}
