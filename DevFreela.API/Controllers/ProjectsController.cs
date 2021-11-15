0404aa87c247cef36e4a08f92b81e29fec741cc0
MateusGouveia <mateusgouveiadev@gmail.com>
2021-11-15 14:46:25 -0300

    Configuracoes de aplicacao

--- DevFreela.API/Controllers/ProjectsController.cs
+++ DevFreela.API/Controllers/ProjectsController.cs
@@ -1,5 +1,6 @@
 ﻿using DevFreela.API.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,6 +11,13 @@ namespace DevFreela.API.Controllers
     [Route("api/projects")]
     public class ProjectsController : ControllerBase
     {
+        private readonly OpeningTimeOption _option;
+
+        public ProjectsController(IOptions<OpeningTimeOption> options)
+        {
+            _option = options.Value;
+        }
+
         // api/projects?query=net core
         [HttpGet]
         public IActionResult Get(string query)
