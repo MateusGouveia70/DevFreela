7dd280e1c58ad5e9ab8af4bbf560a069c76bb023
MateusGouveia <mateusgouveiadev@gmail.com>
2021-11-17 10:05:57 -0300

    Camada core

---
+++ DevFreela.Core/Entities/ProjectComment.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFreela.Core.Entities
+{
+    public class ProjectComment : BaseEntity
+    {
+        public ProjectComment(string content, int idProject, int userId)
+        {
+            Content = content;
+            IdProject = idProject;
+            UserId = userId;
+
+            CreatedAt = DateTime.Now;
+        }
+
+        public string Content { get; private set; }
+        public int IdProject { get; private set; }
+        public int UserId { get; private set; }
+        public DateTime CreatedAt { get; private set; }
+
+    }
+}
